825fbc6623a86f0747b29ef54b8b519c5be794d3
Juan Fernandez <jcfernan@live.com>
2020-04-23 00:14:40 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -3,7 +3,6 @@ def find_min_in_nested_arrays(src)
  row_index = 0
  while row_index < src.length do
    el_index = 0
-   weekly_min = []
    lowest = 1000
    while el_index < src[row_index].length do
      if src[row_index][el_index] < lowest
